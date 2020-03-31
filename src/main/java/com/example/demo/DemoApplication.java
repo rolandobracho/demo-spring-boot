c354682a44333862ba0854b29a0f901024448bce
rolando <patagonia>
2020-03-31 14:30:22 -0300

    ADD: fisrt commit.

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/example/demo/controller/AccionsController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+//import com.patagoniait.puc.bioetica.api.service.AccionesService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+/**
+ * Created by rolando on 31-03-20.
+ */
+@RestController
+@RequestMapping("/accions")
+public class AccionsController {
+
+    @RequestMapping(value = "/hola", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    ResponseEntity hola(@RequestParam("idTest") Long idtest) throws IOException {
+//        accionesService.anularSolicitud(idSolicitud,idMotivo,motivoOtro);
+        ResponseEntity response = new ResponseEntity(HttpStatus.OK);
+        return response;
+    }
+}
