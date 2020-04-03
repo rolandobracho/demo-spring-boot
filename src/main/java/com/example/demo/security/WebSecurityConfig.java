53f0fea5441d7e41e3c1829d411258dd0c8b7a73
rolando <patagonia>
2020-04-03 10:13:09 -0300

    FIX: solo token esta permitido, usar /** para permitir todos los recursos sin jwt.

--- src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/token/").permitAll()
                 .anyRequest().authenticated();
     }
 }
